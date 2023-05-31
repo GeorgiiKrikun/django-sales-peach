f5acd681b832ebac3385f4f4005cec75f8249377
Georgii <georgii.krikun@gmail.com>
2023-05-31 08:24:10 +0200

    Add password reset endpoint

--- src/apps/authentication/forms.py
+++ src/apps/authentication/forms.py
@@ -6,7 +6,8 @@ Copyright (c) 2019 - present AppSeed.us
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
+from django.contrib.auth import password_validation
 
 class LoginForm(forms.Form):
     username = forms.CharField(
@@ -23,6 +24,51 @@ class LoginForm(forms.Form):
                 "class": "form-control"
             }
         ))
+
+class EnterEmailForPasswordResetForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": "Email",
+                "class": "form-control"
+            }
+        ))
+    
+class ResetPasswordForm(forms.Form):
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password",
+                "class": "form-control"
+            }
+        ))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "Password check",
+                "class": "form-control"
+            }
+        ))
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def _post_clean(self):
+        super()._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
+        password = self.cleaned_data.get('password2')
+        if password:
+            try:
+                password_validation.validate_password(password)
+            except ValidationError as error:
+                self.add_error('password2', error)
 
 
 class SignUpForm(UserCreationForm):
--- src/apps/authentication/views.py
+++ src/apps/authentication/views.py
@@ -6,19 +6,18 @@ Copyright (c) 2019 - present AppSeed.us
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, SignUpForm, EnterEmailForPasswordResetForm, ResetPasswordForm
 from core.settings import GITHUB_AUTH
 from django.template import loader
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.encoding import force_str, smart_str, smart_bytes, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 from django.core.mail import EmailMessage
 from django.contrib import messages
-from .tokens import account_activation_token
+from .tokens import account_activation_token,password_reset_token
 from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
@@ -114,7 +113,64 @@ def activateEmail(request, user, to_email):
     else:
         messages.error(request, f'Problem sending confirmation email to {to_email}, check if you typed it correctly.')
 
+def send_reset_password_email(request, user, to_email):
+    mail_subject = 'Reset your password.'
+    message = render_to_string('authentication/reset_password_email.html', {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': password_reset_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
+            received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
+    else:
+        messages.error(request, f'Problem sending confirmation email to {to_email}, check if you typed it correctly.')
 
 
 def reset_password(request):
-    return render(request, "authentication/auth-reset-password.html", {})
+    if request.method == "GET":
+        form  = EnterEmailForPasswordResetForm()
+    elif request.method == "POST":
+        form = EnterEmailForPasswordResetForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            user = User.objects.get(email=email)
+            send_reset_password_email(request, user, email)
+            return redirect(reverse('authentication:login'))
+    return render(request, "authentication/auth-reset-password.html", {'form': form})
+
+def reset_password_link(request, uidb64, token):
+    if request.method == "GET":       
+        form = ResetPasswordForm()
+        return render(request, "authentication/auth-reset-password.html", {'form': form})
+    elif request.method == "POST":
+        form = ResetPasswordForm(request.POST)
+        if form.is_valid():
+            password1 = form.cleaned_data.get("password1")
+            password2 = form.cleaned_data.get("password2")
+            try:
+                uid = force_str(urlsafe_base64_decode(uidb64))
+                user = User.objects.get(pk=uid)
+            except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+                messages.error(request, 'Invalid activation link')
+                
+            if user is not None and password_reset_token.check_token(user, token):
+                if password1 != password2:
+                    messages.error(request, 'Passwords do not match')
+                    return redirect(reverse('authentication:login'))
+                user.set_password(password1)
+                user.save()
+                messages.success(request, 'Password reset successful')
+                return redirect(reverse('authentication:login'))
+            else:
+                messages.error(request, 'Password reset failed')
+                return redirect(reverse('authentication:login'))
+        else:
+            messages.error(request, 'Password reset failed')
+            return render(request, "authentication/auth-reset-password.html", {'form': form})
+         
+
+    return redirect('authentication:login')
